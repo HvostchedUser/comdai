fe26879a400f492665e278a86bcc58a71880c1ef
hvostcheduser <yogik_iz_gretsii@mail.ru>
2024-09-05 08:59:35 +0300

    Shrink the text to fit context of the model

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-##TODO: Math and code inlines and blocks should not be parsed!
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 import diff_match_patch as dmp_module
@@ -14,7 +13,7 @@ llm = Llama(
     model_path="gemma-2-2b-it-Q5_K_M.gguf",
     verbose=False,
     n_threads=8,
-    n_ctx=8128,
+    n_ctx=8128,  # Set the max context to 256 tokens
     n_batch=1024,
     n_gpu_layers=1000
 )
@@ -26,7 +25,38 @@ dmp = dmp_module.diff_match_patch()
 turn_system = "<start_of_turn>user\n"
 turn_user = "<end_of_turn>\n<start_of_turn>user\n"
 turn_assistant = "<end_of_turn>\n<start_of_turn>model\n"
-prompt_base = f"{turn_system}You help to write conspect. You can use tables, blockquotes, lists, formulas (in the following format: $<math>$), code, and other formatting. Be conscise, answer as shorlty as possible, use formatting where possible. Answer without referencing user, just proceed to request"
+prompt_base = f"{turn_system}You help to write conspect. You can use tables, blockquotes, lists, formulas (in the following format: $<math>$), code, and other formatting. Be concise, answer as shortly as possible, use formatting where possible. Answer without referencing user, just proceed to request"
+
+
+def truncate_context(context, prompt, max_context_length=256, before_ratio=0.8, after_ratio=0.2):
+    tokens = llm.tokenize(context.encode("utf-8"))
+    # print(len(tokens))
+    prompt_tokens = llm.tokenize(prompt.encode("utf-8"))
+    # if len(prompt_tokens)>2:
+    #     prompt_tokens = prompt_tokens[1: -1]
+    if len(tokens) + len(prompt_tokens) <= max_context_length:
+        return context  # No need to truncate
+
+    # print("NEED TO TRUNCATE")
+    prompt_index = context.find(prompt)
+    # print(context, prompt)
+    # print(prompt_index)
+    if prompt_index == -1:
+        # print("CANNOT FIND PROMPT")
+        return context  # Prompt not found, returning full context
+
+    # Determine truncation range
+    before_prompt = int(max_context_length * before_ratio)
+    after_prompt = max_context_length - before_prompt
+
+    prompt_start_token = len(llm.tokenize(context[:prompt_index].encode("utf-8")))
+    start_idx = max(0, prompt_start_token - before_prompt)
+    end_idx = min(len(tokens), prompt_start_token + len(prompt_tokens) + after_prompt)
+
+    truncated_tokens = tokens[start_idx:end_idx]
+
+    truncated_context = llm.detokenize(truncated_tokens)
+    return truncated_context
 
 
 @app.route('/')
@@ -57,38 +87,34 @@ def handle_markdown_patch(data):
 def handle_process_special_format(data):
     sender_id = data['clientId']
     special_lines = data['matches']
-    # print(data)
-    # print(special_lines)
-    for line in special_lines:
-        # Prepare the prompt for the language model
-        user_text = line
-        prompt = prompt_base + "\n" + shared_content[
-            'markdown'] + f"\n\nUser request regarding the text:\n{user_text}{turn_assistant}"
 
-        # Create an initial patch to remove the special format line and replace with placeholder
+    for line in special_lines:
+        user_text = line
         current_markdown = shared_content['markdown']
+        prompt = f"\n\nUser request regarding the text:\n{user_text}{turn_assistant}"
+
+        # Truncate the context
+        truncated_context = truncate_context(shared_content['markdown'], f"=\\{user_text}\\", max_context_length=2048)
+        full_prompt = f"{prompt_base}\n...{truncated_context}...{prompt}"
+        print(full_prompt)
         placeholder = "=> "
         updated_markdown = current_markdown.replace(f'=\\{line}\\', placeholder, 1)
-        # print(current_markdown, updated_markdown)
         diffs = dmp.diff_main(current_markdown, updated_markdown)
         if diffs:
             dmp.diff_cleanupSemantic(diffs)
             patches = dmp.patch_make(current_markdown, diffs)
             patch_text = dmp.patch_toText(patches)
 
-            # Update the shared content with the placeholder
             shared_content['markdown'] = updated_markdown
-
-            # Send the initial patch with the placeholder
             emit('patch_update', {'patchText': patch_text, 'senderId': 'llm'}, broadcast=True)
-            socketio.sleep(0)  # Small sleep to let the event loop emit the message
+            socketio.sleep(0)
 
         # Generate text using the language model
         full_str = ""
-        print(prompt)
+        # print(full_prompt)
         for output in llm(
-                prompt,
-                max_tokens=8128,
+                full_prompt,
+                max_tokens=2048,
                 stop=["<|end|>", "<end_of_turn>", "<|eot_id|>"],
                 stream=True,
                 temperature=0.5,
@@ -98,7 +124,7 @@ def handle_process_special_format(data):
         ):
             token_str = output["choices"][0]["text"]
             print(token_str, end="", flush=True)
-            # Update the placeholder in real-time
+
             current_markdown = shared_content['markdown']
             if len(full_str) == 0:
                 updated_markdown = current_markdown.replace(placeholder + full_str,
@@ -106,8 +132,8 @@ def handle_process_special_format(data):
             else:
                 updated_markdown = current_markdown.replace(placeholder + full_str + "<=|",
                                                             placeholder + full_str + token_str + "<=|", 1)
-            print(current_markdown, updated_markdown)
-            # print(updated_markdown)
+            # print(current_markdown, updated_markdown)
+
             diffs = dmp.diff_main(current_markdown, updated_markdown)
             if diffs and (("<=|" in current_markdown and "=>" in current_markdown) or len(full_str) == 0):
                 full_str += token_str
@@ -115,18 +141,14 @@ def handle_process_special_format(data):
                 patches = dmp.patch_make(current_markdown, diffs)
                 patch_text = dmp.patch_toText(patches)
 
-                # Update the shared content with the current generated text
                 shared_content['markdown'] = updated_markdown
-
-                # Stream the patch update to the frontend
                 emit('patch_update', {'patchText': patch_text, 'senderId': "llm"}, broadcast=True)
-                socketio.sleep(0.1)  # Small sleep to let the event loop emit the message
+                socketio.sleep(0.1)
             else:
                 print("BREAK")
                 break
 
         current_markdown = shared_content['markdown']
-        # print(current_markdown)
         updated_markdown = current_markdown.replace(placeholder + full_str + token_str + "<=|=", full_str, 1)
         diffs = dmp.diff_main(current_markdown, updated_markdown)
         if diffs:
@@ -134,14 +156,10 @@ def handle_process_special_format(data):
             patches = dmp.patch_make(current_markdown, diffs)
             patch_text = dmp.patch_toText(patches)
 
-            # Update the shared content with the current generated text
             shared_content['markdown'] = updated_markdown
-
-            # Stream the patch update to the frontend
             emit('patch_update', {'patchText': patch_text, 'senderId': "llm"}, broadcast=True)
-            socketio.sleep(0)  # Small sleep to let the event loop emit the message
+            socketio.sleep(0)
     current_markdown = shared_content['markdown']
-    # print(current_markdown)
     updated_markdown = current_markdown.replace(placeholder + full_str + token_str + "<=|", full_str, 1)
     diffs = dmp.diff_main(current_markdown, updated_markdown)
     if diffs:
@@ -149,12 +167,9 @@ def handle_process_special_format(data):
         patches = dmp.patch_make(current_markdown, diffs)
         patch_text = dmp.patch_toText(patches)
 
-        # Update the shared content with the current generated text
         shared_content['markdown'] = updated_markdown
-
-        # Stream the patch update to the frontend
         emit('patch_update', {'patchText': patch_text, 'senderId': "llm"}, broadcast=True)
-        socketio.sleep(0)  # Small sleep to let the event loop emit the message
+        socketio.sleep(0)
 
 
 if __name__ == '__main__':
